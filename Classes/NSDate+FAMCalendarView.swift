131fc1cb06ab6f01b394342a7dcfaebb1fcea127
fromKK <info@fromkk.info>
2016-03-27 21:33:43 +0900

    move NSDate protocol

---
+++ Classes/NSDate+FAMCalendarView.swift
@@ -0,0 +1,60 @@
+//
+//  NSDate+FAMCalendarView.swift
+//  FAMCalendarView
+//
+//  Created by Kazuya Ueoka on 2016/03/27.
+//  Copyright © 2016年 fromKK. All rights reserved.
+//
+
+import Foundation
+
+public protocol NSDateFAMCalendarViewProtocol
+{
+    func dateFromIndexPath(indexPath :NSIndexPath) -> NSDate?
+    func day() -> Int
+    func isEqual(toYear year :Int, month :Int) -> Bool
+    static func date(fromYear year :Int, month :Int, day :Int) -> NSDate
+}
+
+extension NSDate :NSDateFAMCalendarViewProtocol
+{
+    public func dateFromIndexPath(indexPath: NSIndexPath) -> NSDate? {
+        let calendar :NSCalendar = NSCalendar.currentCalendar()
+        calendar.locale = NSLocale.systemLocale()
+        calendar.timeZone = NSTimeZone.systemTimeZone()
+        
+        let comp1 :NSDateComponents = calendar.components([.Year, .Month], fromDate: self)
+        let comp2 :NSDateComponents = NSDateComponents()
+        comp2.weekOfMonth = indexPath.section + 1
+        comp2.weekday     = indexPath.row + 1
+        comp2.year        = comp1.year
+        comp2.month       = comp1.month
+        return calendar.dateFromComponents(comp2)
+    }
+    
+    public func day() -> Int {
+        let calendar :NSCalendar = NSCalendar.currentCalendar()
+        calendar.locale = NSLocale.systemLocale()
+        calendar.timeZone = NSTimeZone.systemTimeZone()
+        return calendar.component(.Day, fromDate: self)
+    }
+    
+    public func isEqual(toYear year: Int, month: Int) -> Bool {
+        let calendar :NSCalendar = NSCalendar.currentCalendar()
+        calendar.locale = NSLocale.systemLocale()
+        calendar.timeZone = NSTimeZone.systemTimeZone()
+        let components :NSDateComponents = calendar.components([.Year, .Month], fromDate: self)
+        return components.year == year && components.month == month
+    }
+    
+    public static func date(fromYear year :Int, month :Int, day :Int) -> NSDate {
+        let components :NSDateComponents = NSDateComponents()
+        components.year = year
+        components.month = month
+        components.day = day
+        let calendar :NSCalendar = NSCalendar.currentCalendar()
+        calendar.locale = NSLocale.systemLocale()
+        calendar.timeZone = NSTimeZone.systemTimeZone()
+        return calendar.dateFromComponents(components)!
+    }
+}
